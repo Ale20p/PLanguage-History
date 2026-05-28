973afc13761533d8f7b103aa593f4235801f63e0
Alessandro Pomponi <alescode1@gmail.com>
2026-05-28 00:56:05 -0400

    update config to support building of static site on web host

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Forces Next.js to generate static files
+  images: {
+    unoptimized: true, // Required for static export images
+  },
 };
 
 export default nextConfig;
